743d2296b9cfc508d300eca399eedf8f29abe3a8
tebecheri10 <tebecheri10@gmail.com>
2022-10-11 13:44:55 +0200

    infite-scroll agregado

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
+
+let imagesContainer = document.querySelector("#images-container");
+let loader = document.querySelector(".loader-container")
+let img = null
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 //unsplash API
-const apiKey = 'gvqTXoFaZYuvI8UVSOwGVVfuGRIeDoas3OPFsq2RaeM'
-const photosQty = 20
-const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photosQty}`
+const API_KEY = 'FgNVUZcPtOKEm-5oHjcZbcrLKMcFn9-g5pEHs8MMBD0';
+const photosQty = 20;
+const url = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${photosQty}`
+
+const loadedImages = ()=>{
+    imagesLoaded++
+    if (imagesLoaded === totalImages){
+        ready = true
+        loader.style.display = "none"
+    }
+}
+
+
+
+const infiniteScroll = ()=>{
+    window.addEventListener("scroll", ()=>{
+        if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+           ready = false
+           getPhotos()
+        }
+    })
+}
+
+const mapImages = ()=>{
+    imagesLoaded = 0
+    totalImages = photosArray.length;
+    photosArray.forEach((photo)=>{
+        imagesContainer.insertAdjacentHTML("afterbegin", `<a class="photo-link" href="${photo.links.html}"><img class="gallery-image" src="${photo.urls.regular}" alt="${photo.alt_description}"/><a>`)
+    })
+}
 
 const getPhotos = async ()=>{
     try {
        const response = await fetch(url);
-       const data = await response.json();
-       console.log("data", data)
+       photosArray = await response.json();
+       mapImages()
+       img = document.querySelectorAll(".gallery-image")
+       img.forEach((img)=>{
+        img.addEventListener("load", loadedImages)
+    })
     } catch (error) {
         console.log( "ha ocurrido un error : ", error)
     }
 }
 
 getPhotos();
+infiniteScroll()
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,5 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: "development",
-  watch: true,
+  watch: true
 };
