1370e69b04223fb5b023cf6ecb0b90d5d3d01b8e
tebecheri10 <tebecheri10@gmail.com>
2022-10-12 12:31:49 +0200

    webpack config update / .env configured

--- index.js
+++ index.js
@@ -1,57 +1,64 @@
+import css from './styles.css'
 
 let imagesContainer = document.querySelector("#images-container");
-let loader = document.querySelector(".loader-container")
-let img = null
+let loader = document.querySelector(".loader-container");
+let img = null;
 let photosArray = [];
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 
 //unsplash API
-const API_KEY = 'FgNVUZcPtOKEm-5oHjcZbcrLKMcFn9-g5pEHs8MMBD0';
 const photosQty = 20;
-const url = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${photosQty}`
+const url = `https://api.unsplash.com/photos/random/?client_id=${process.env.API_KEY}&count=${photosQty}`;
 
-const loadedImages = ()=>{
-    imagesLoaded++
-    if (imagesLoaded === totalImages){
-        ready = true
-        loader.style.display = "none"
+const loadedImages = () => {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.style.display = "none";
+  }
+};
+
+const infiniteScroll = () => {
+  window.addEventListener("scroll", () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000 &&
+      ready
+    ) {
+      ready = false;
+      getPhotos();
     }
-}
+  });
+};
 
+const mapImagesOnScreen = () => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  photosArray.forEach((photo) => {
+    imagesContainer.insertAdjacentHTML(
+      "afterbegin",
+      `<a class="photo-link" target="_blank" href="${photo.links.html}"><img class="gallery-image" src="${photo.urls.regular}" alt="${photo.alt_description}"/><a>`
+    );
+  });
+};
 
+const getPhotos = async () => {
+  try {
+    const response = await fetch(url);
+    photosArray = await response.json();
+    mapImagesOnScreen();
+    img = document.querySelectorAll(".gallery-image");
+    img.forEach((img) => {
+      img.addEventListener("load", loadedImages);
+    });
+  } catch (error) {
+    console.log("ha ocurrido un error : ", error);
+  }
+};
 
-const infiniteScroll = ()=>{
-    window.addEventListener("scroll", ()=>{
-        if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
-           ready = false
-           getPhotos()
-        }
-    })
-}
-
-const mapImages = ()=>{
-    imagesLoaded = 0
-    totalImages = photosArray.length;
-    photosArray.forEach((photo)=>{
-        imagesContainer.insertAdjacentHTML("afterbegin", `<a class="photo-link" href="${photo.links.html}"><img class="gallery-image" src="${photo.urls.regular}" alt="${photo.alt_description}"/><a>`)
-    })
-}
-
-const getPhotos = async ()=>{
-    try {
-       const response = await fetch(url);
-       photosArray = await response.json();
-       mapImages()
-       img = document.querySelectorAll(".gallery-image")
-       img.forEach((img)=>{
-        img.addEventListener("load", loadedImages)
-    })
-    } catch (error) {
-        console.log( "ha ocurrido un error : ", error)
-    }
-}
-
-getPhotos();
-infiniteScroll()
+window.addEventListener("load", () => {
+  getPhotos();
+  infiniteScroll();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,43 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require("dotenv-webpack");
+const { WEBPACK_IGNORE_COMMENT_REGEXP } = require('css-loader/dist/utils');
 
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+},
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, "css-loader"],
+    },
+    {
+      test: /\.png/,
+      type: 'asset/resource',
+    }
+  ],
+},
   mode: "development",
-  watch: true
+  plugins:[
+    new MiniCssExtractPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new Dotenv()
+  ]
 };
